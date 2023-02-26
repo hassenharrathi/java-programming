fa27e92969843c3689cc95bdd791959c9f734305
hassenharrathi <118659746+hassenharrathi@users.noreply.github.com>
2023-02-26 14:08:18 +0100

    Update DayOfTheWeek.java
    I think the application should first make sure that the entry is correct. So we need to create a method to do this.

--- exercises/chapter7/DayOfTheWeek.java
+++ exercises/chapter7/DayOfTheWeek.java
@@ -2,16 +2,30 @@ package exercises.chapter7;
 
 import java.util.Scanner;
 
-public class DayOfTheWeek {
-
+public class daysOfWeek {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String args[]){
-        String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        String[] week = {"Monday", "Thusday", "Wendesday", "Thirsday", "Friday", "Saturday", "Sunday"};
+        int number = getNumber();
+        System.out.println("The day of the week is :" +getDay(number, week));
+    }
+    public static int getNumber(){
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number for the day of the week");
-        int index = input.nextInt();
-        input.close();
+        System.out.println("Enter a number of day: ");
+        int number = scanner.nextInt();
+        if(number > 7 || number < 1) {
+            do {
+                System.out.println("The number must be between 1 and 7, tray again: ");
+                number = scanner.nextInt();
+            } while ((number > 7) || (number < 1));
 
-        System.out.println("Corresponding day: " + week[index - 1]);
+        }
+
+        return number;
+    }
+    public static String getDay(int number, String week[]){
+        String day = week[number-1];
+        return day;
     }
 }
+
